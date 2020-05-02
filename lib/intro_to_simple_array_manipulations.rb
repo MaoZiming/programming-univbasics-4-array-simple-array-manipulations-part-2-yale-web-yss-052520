480f75d6d6269dfe28dae67bca6b4d48d662a92b
Ziming Mao <ziming.mao@yale.edu>
2020-05-02 23:40:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,3 +7,24 @@ def using_concat(first, second)
   end
   first
 end
+
+def using_insert(array, element)
+  array.insert(4, element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, element)
+  array.delete(element)
+end
+
+def using_delete_at(array, index)
+  array.delete_at(index)
+end
+  
